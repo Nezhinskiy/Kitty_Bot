4b82faf0f6c040d45869ae769eefe34319f7c030
Mikhail Nezhinsky <m.nezhinsky@yandex.ru>
2022-06-27 13:30:50 +0300

    add if main

--- kittybot.py
+++ kittybot.py
@@ -43,8 +43,14 @@ def wake_up(update, context):
     context.bot.send_photo(chat.id, get_new_image())
 
 
-updater.dispatcher.add_handler(CommandHandler('start', wake_up))
-updater.dispatcher.add_handler(CommandHandler('newcat', new_cat))
+def main():
+    updater = Updater(token=secret_token)
+    updater.dispatcher.add_handler(CommandHandler('start', wake_up))
+    updater.dispatcher.add_handler(CommandHandler('newcat', new_cat))
 
-updater.start_polling()
-updater.idle()
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
